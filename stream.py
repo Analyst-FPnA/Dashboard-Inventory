8b96704e996edab9407c477e57469cece155f827
Analyst-FPnA <analyst.fpna@gmail.com>
2024-08-23 16:46:53 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -123,6 +123,6 @@ df_4101_2.columns = ['_'.join(col).strip() for col in df_4101_2.columns.values]
 df_4101_2.iloc[:,4:] = df_4101_2.iloc[:,4:].astype(float)
 total = pd.DataFrame((df_4101_2.iloc[:,3:].sum(axis=0).values).reshape(1,len(df_4101_2.columns)-3),columns=df_4101_2.columns[3:])
 total['Nama Barang_']='TOTAL'
-
+df_4101_2.iloc[:,3:] = df_4101_2.iloc[:,3:].astype(int)
 #df_4101_2.iloc[:,3:] = df_4101_2.iloc[:,3:].applymap(lambda x: '' if x=='' else f'{x:,.0f}')
 st.dataframe(pd.concat([df_4101_2,total]).fillna(''), use_container_width=True, hide_index=True)
